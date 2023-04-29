11b61a17f234a7e24640f38cda7f7ed153607f22
xist-cloud <anujmali450@gmail.com>
2023-04-29 10:46:34 +0545

    Installed Axios

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
+const User = require("../models/userModel");
 
 router.get('/test', (req,res) => {
     res.send("Welcome");
 });
 
-router.post('/register', (req, res)=>{
-    console.log(req.body);
+router.post('/register', async (req, res)=>{
+    // console.log(req.body);
+
+    // unpacking
+    const {fname, lname, email, password} = req.body;
+
+    // validation
+    if(!(fname && lname && email && password)){
+        return res.status(400).json({msg: "All fields are required"})
+    }
+
+    try {
+        // check existing user
+        const userExists = await User.exists({email});
+
+        if(userExists){
+            return res.status(400).json({msg: "User already exists."})
+        }
+
+        const newUser = new User(req.body);
+        newUser.save()
+
+        res.json({msg: "User registered successfully"})
+
+    } catch (error) {
+        res.status(500).json({msg:"User registration failed"})
+    }
+
     res.send('Register');
 })
 
