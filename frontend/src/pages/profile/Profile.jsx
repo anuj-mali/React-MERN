f450ea4f352b0079a153ca68b93e55da825c8f9e
xist-cloud <anujmali450@gmail.com>
2023-05-21 08:21:04 +0545

    Added Profile

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Profile = () => {
-    return <div>Profile</div>;
+    const user = JSON.parse(localStorage.getItem("user"));
+    return (
+        <div className="container d-flex flex-column align-items-center mt-3">
+            <div className="d-flex flex-column align-items-center">
+                <img
+                    src="https://www.picsum.photos/200"
+                    alt=""
+                    width={300}
+                    className="rounded-circle"
+                />
+                <h3>Welcome, {user.fname}</h3>
+            </div>
+
+            <div className="mt-5">
+                <h4>Personal Information</h4>
+                <p>Name: {user.fname + " " + user.lname}</p>
+                <p>Email: {user.email}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
